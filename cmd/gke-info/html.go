4760ff6506b71623d42f6d1781efe377bb9a4648
Fábio Silva <aragao.silva@gmail.com>
2018-07-12 16:41:49 +1000

    Add files via upload

--- cmd/gke-info/html.go
+++ cmd/gke-info/html.go
@@ -24,7 +24,6 @@ const (
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
 
 <!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
 <title>Frontend Web Server</title>
 </head>
 <body>
